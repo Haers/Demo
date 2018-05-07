c74243df7112aacf2a634168bcdad5b0534f5d15
XH961209 <3024693372@qq.com>
2018-05-07 15:16:51 +0800

    添加一个TextView，用于描述消息 设置帮助按钮的点击事件

--- app/src/main/java/com/acticitytest/demo/ItemActivity.java
+++ app/src/main/java/com/acticitytest/demo/ItemActivity.java
@@ -1,7 +1,11 @@
 package com.acticitytest.demo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,5 +23,28 @@ public class ItemActivity extends AppCompatActivity {
         imageView.setImageResource(picId);
         TextView textView=(TextView)findViewById(R.id.itemName);
         textView.setText(countryName);
+
+        Button button=(Button)findViewById(R.id.help);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(ItemActivity.this);
+                dialog.setMessage("确定帮助？");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 }
