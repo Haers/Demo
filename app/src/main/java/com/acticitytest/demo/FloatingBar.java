5bdea140e9d83eb0ae121f8f50ce6bcdba5abdd4
XH961209 <3024693372@qq.com>
2018-05-08 16:02:17 +0800

    登录界面作为main activity

--- app/src/main/java/com/acticitytest/demo/FloatingBar.java
+++ app/src/main/java/com/acticitytest/demo/FloatingBar.java
@@ -2,6 +2,7 @@ package com.acticitytest.demo;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -35,51 +36,23 @@ public class FloatingBar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*android.support.v4.app.FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(android.R.id.content,new ListViewFragment());*/
         initActionBar();
+
     }
-    @SuppressWarnings("deprecation")
+   @SuppressWarnings("deprecation")
     private void initActionBar() {
         if (getSupportActionBar()    != null) {
             ActionBar actionBar = getSupportActionBar();
             actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.hide();
             actionBar.addTab(actionBar.newTab()
-                    .setText("ListView")
                     .setTabListener(new ActionBar.TabListener() {
                         @Override
                         public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
                             fragmentTransaction.replace(android.R.id.content, new ListViewFragment());
-                        }
-
-                        @Override
-                        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                        }
-
-                        @Override
-                        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                        }
-                    }));
-            actionBar.addTab(actionBar.newTab()
-                    .setText("RecyclerView")
-                    .setTabListener(new ActionBar.TabListener() {
-                        @Override
-                        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                            fragmentTransaction.replace(android.R.id.content, new RecyclerViewFragment());
-                        }
-
-                        @Override
-                        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                        }
-
-                        @Override
-                        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                        }
-                    }));
-            actionBar.addTab(actionBar.newTab()
-                    .setText("ScrollView")
-                    .setTabListener(new ActionBar.TabListener() {
-                        @Override
-                        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                            fragmentTransaction.replace(android.R.id.content, new ScrollViewFragment());
                         }
 
                         @Override
@@ -174,55 +147,6 @@ public class FloatingBar extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
-
-            return root;
-        }
-    }
-
-    public static class RecyclerViewFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View root = inflater.inflate(R.layout.fragment_recyclerview, container, false);
-
-            RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.recycler_view);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setItemAnimator(new DefaultItemAnimator());
-            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-            recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
-
-            RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity(), getResources()
-                    .getStringArray(R.array.countries));
-            recyclerView.setAdapter(adapter);
-
-            FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
-            fab.attachToRecyclerView(recyclerView);
-
-            return root;
-        }
-    }
-
-    public static class ScrollViewFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View root = inflater.inflate(R.layout.fragment_scrollview, container, false);
-
-            ObservableScrollView scrollView = (ObservableScrollView) root.findViewById(R.id.scroll_view);
-            LinearLayout list = (LinearLayout) root.findViewById(R.id.list);
-
-            String[] countries = getResources().getStringArray(R.array.countries);
-            for (String country : countries) {
-                TextView textView = (TextView) inflater.inflate(R.layout.list_item, container, false);
-                String[] values = country.split(",");
-                String countryName = values[0];
-                int flagResId = getResources().getIdentifier(values[1], "drawable", getActivity().getPackageName());
-                textView.setText(countryName);
-                textView.setCompoundDrawablesWithIntrinsicBounds(flagResId, 0, 0, 0);
-
-                list.addView(textView);
-            }
-
-            FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
-            fab.attachToScrollView(scrollView);
 
             return root;
         }
